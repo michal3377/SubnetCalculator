e540074613022a2f831408a2508505610f0bfecc
michal3377 <michal3377@gmail.com>
2019-03-27 21:39:20 +0100

    Add calculations for IP address properties

--- SubnetCalculator/Net/MyIPAddress.cs
+++ SubnetCalculator/Net/MyIPAddress.cs
@@ -4,44 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SubnetCalculator.Net
-{
+namespace SubnetCalculator.Net {
     public class MyIPAddress {
-
-        public enum AddressClass {
-            A, B, C, D, E
+        public enum AddressClassType {
+            A,
+            B,
+            C,
+            D,
+            E
         }
         /**
-         * Jedyna modyfikacja (get/set) przez zmiane AddressValue, Octet (przy set warunek na size==4), Maska.
+         * Jedyna modyfikacja (get/set) przez zmiane Value, Octet (przy set warunek na size==4), Maska.
          * Metoda update
          */
 
-        public UInt32 AddressValue;
+        public UInt32 Value;
         public byte[] Octets = {0, 0, 0, 0};
 
 
         private int _maskSize = 0;
+
         public int MaskSize {
             get => _maskSize;
             set {
-                if (value < 0 || value > 32) throw new ArgumentOutOfRangeException(nameof(MaskSize),
-                    "Mask size must be within <1-32> range");
+                if (value < 0 || value > 32)
+                    throw new ArgumentOutOfRangeException(nameof(MaskSize),
+                        "Mask size must be within <0-32> range");
                 _maskSize = value;
+                Mask = new MyIPAddress(CalculateMaskInteger());
             }
         }
 
-        public UInt32 MaskValue => CalculateMaskInteger();
         public MyIPAddress Mask;
 
-
-        public MyIPAddress Subnet;
+        public AddressClassType AddressClass;
+        public bool BelongsToPublicPool;
+        public MyIPAddress Subnet { get; private set; }
+        public MyIPAddress Broadcast { get; private set; }
+        public MyIPAddress FirstHostAddress { get; private set; }
+        public MyIPAddress LastHostAddress { get; private set; }
+        public int MaxHostAmount { get; private set; }
 
         public MyIPAddress() {
         }
 
-        public MyIPAddress(uint addressValue) {
-            AddressValue = addressValue;
+        public MyIPAddress(uint value) {
+            Value = value;
         }
+
+        private void UpdateValues() {
+            Mask = new MyIPAddress(CalculateMaskInteger());
+            Subnet = new MyIPAddress(CalculateSubnetAddress());
+            Broadcast = new MyIPAddress(CalculateBroadcastAddress());
+            FirstHostAddress = new MyIPAddress(CalculateFirstHostAddress());
+            LastHostAddress = new MyIPAddress(CalculateLastHostAddress());
+            MaxHostAmount = CalculateMaxHostAmount();
+            
+        }
+
 
         public static MyIPAddress Parse(string address) {
             var ip = new MyIPAddress();
@@ -58,20 +78,38 @@ namespace SubnetCalculator.Net
             return ip;
         }
 
+        //1st index from the right side, 0 indexed
+        public uint GetOctetValue(int octetIndex) {
+            uint mask = 0xFF;
+            return mask & (Value >> (octetIndex * 8));
+        }
+
 
         private UInt32 CalculateMaskInteger() {
-            return UInt32.MaxValue << 32 - MaskSize; 
+            int a = 0xFFFFFFF;
+            return UInt32.MaxValue << 32 - MaskSize;
         }
 
-        private MyIPAddress CalculateSubnetAddress() {
-            return new MyIPAddress(MaskValue & AddressValue);
+        private UInt32 CalculateSubnetAddress() {
+            return Mask.Value & Value;
         }
 
-        private MyIPAddress CalculateFirstHostAddress() {
-            return new MyIPAddress(Subnet.AddressValue + 1);
+        private UInt32 CalculateFirstHostAddress() {
+            return Subnet.Value + 1;
         }
 
 
+        private UInt32 CalculateBroadcastAddress() {
+            return ~Mask.Value | Value;
+        }
 
+        private UInt32 CalculateLastHostAddress() {
+            return Broadcast.Value + 1;
+        }
+
+        private int CalculateMaxHostAmount() {
+            var am = Convert.ToInt32((~Mask.Value) - 2);
+            return am >= 0 ? am : 0;
+        }
     }
 }
